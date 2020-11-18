19a7af3ac615787984340770f90228f77c3a005f
Chris Bullard <jira.christopher@bullard.me.uk>
2020-11-18 21:47:07 +0000

    Add bootstrap styling to project table

--- client/src/components/projects/projecttable.tsx
+++ client/src/components/projects/projecttable.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import IProjectModel from '../../models/interfaces/IProjectModel';
 import ProjectRow from './projectrow';
+import Table from 'react-bootstrap/Table';
 
 interface ProjectTableProps {
   projects: IProjectModel[]
@@ -11,19 +12,20 @@ class ProjectTable extends React.Component<ProjectTableProps> {
     const projects = this.props.projects;
 
     return (
-      projects.map((project:IProjectModel) => (
-        <table>
+        <Table striped bordered hover>
           <thead>
             <th>Name</th>
             <th>Description</th>
             <th>Status</th>
           </thead>
           <tbody>
+           
+          {projects.map((project:IProjectModel) => (
             <ProjectRow project={project} />
+          ))}
           </tbody>
-        </table>
-      ))
-    )
+        </Table>
+      )
   }
 }
 
